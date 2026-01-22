34e177df9b5b3de746f2b09da935e5a4ec29ff86
Atul5486 <atulkoushal5486@gmail.com>
2026-01-22 14:25:28 +0530

    Authentication and authorization

---
+++ Authentication and authorization/model/user.js
@@ -0,0 +1,10 @@
+const mongooes=require("mongoose")
+mongooes.connect('mongodb://127.0.0.1:27017/authuser')
+
+const userSchema=mongooes.Schema({
+    username:String,
+    email:String,
+    password:String,
+    age:Number
+})
+module.exports=mongooes.model("user",userSchema)
